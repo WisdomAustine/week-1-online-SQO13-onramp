09d6d380a8d3d9ca73f5794782df621cc79e0fac
WisdomAustine <116816032+WisdomAustine@users.noreply.github.com>
2022-10-31 18:55:49 +0100

    Update challenge-4.js

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,24 @@
 function calculateGrade(marks) {
- 
+    const noOfCourses = marks.length;
+    
+    const total = marks.reduce((prevVal, currentVal) => prevVal + currentVal);
+    const avgScore = total / noOfCourses;
+    console.log(avgScore);
+
+    if(avgScore >= 90 && avgScore <= 100) {
+        return "A";
+    } else if(avgScore >= 80 && avgScore <= 89) {
+        return "B";
+    } else if(avgScore >= 70 && avgScore <= 79) {
+        return "C";
+    } else if(avgScore >= 60 && avgScore <= 69) {
+        return "D";
+    } else if(avgScore >= 50 && avgScore <= 59) {
+        return "E";
+    } else if(avgScore >= 1 && avgScore <= 49) {
+        return "F";
+    } else {
+        return "Invalid average Score";
+    }
  }
   module.exports =calculateGrade
