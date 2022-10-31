c127ba6bfa4a9612606e6d0372a3ccc97b763078
WisdomAustine <116816032+WisdomAustine@users.noreply.github.com>
2022-10-31 18:55:23 +0100

    Update challenge-3.js

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,17 @@
 function sumMix(arr) {
-   
+        let init = 0;
+        const result = arr.map(item => {
+            if(typeof item === "string") {
+                return parseInt(item);
+            } else {
+                return item;
+            }
+        })
+    
+        result.forEach(num => {
+            init += num;
+        })
+        return init;   
  }
 
   module.exports = sumMix
