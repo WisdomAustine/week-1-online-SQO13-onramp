76d297fa3fa03552b899f018cbe00788cd2ce9d3
WisdomAustine <116816032+WisdomAustine@users.noreply.github.com>
2022-10-31 18:54:46 +0100

    Update challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,7 +1,25 @@
 function fizzBuzz(input){
-
+    if(input % 3 === 0) {
+        return "fizz";
+    } else if(input % 5 === 0) {
+        return "buzz";
+    } else if(input % 3 === 0 && input % 5 === 0) {
+        return "fizzbuzz";
+    } else {
+        return input;
+    }
   
 }
 
 
 module.exports = fizzBuzz
+
+function filterArray(array) {
+    const nArray = [];
+    for (let data of array) {
+        if(Number(data)) {
+            nArray.push(data);
+        }
+    }
+    return nArray;
+}
