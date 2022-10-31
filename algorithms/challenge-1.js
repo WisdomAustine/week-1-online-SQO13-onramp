918a583fbabe7b855bdb7646acf290063653c7fd
WisdomAustine <116816032+WisdomAustine@users.noreply.github.com>
2022-10-31 18:53:42 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,12 @@
 function countTruthy(arr) {
+    const array = arr;
+    let count = 0;
 
+    for(let item of array) {
+        if(item) {
+            count++;
+        }
+    }
+    return count;
 }
 module.exports = countTruthy
